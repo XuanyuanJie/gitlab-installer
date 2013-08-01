77dedb229892010843421b47fcfbd88f02c0d064
shake <shake.chen@gmail.com>
2013-08-01 19:40:26 +0800

    Update gitlab-install-el6.sh

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -35,9 +35,9 @@ curl -L get.rvm.io | bash -s stable
 ## Load RVM
 source /etc/profile.d/rvm.sh
 sed -i 's!ftp.ruby-lang.org/pub/ruby!ruby.taobao.org/mirrors/ruby!' /usr/loca/rvm/config/db
-gem sources --remove https://rubygems.org/
-gem sources -a http://ruby.taobao.org/
-gem sources -l
+#gem sources --remove https://rubygems.org/
+#gem sources -a http://ruby.taobao.org/
+#gem sources -l
 ## Fix for missing psych
 ## *It seems your ruby installation is missing psych (for YAML output).
 ## *To eliminate this warning, please install libyaml and reinstall your ruby.
